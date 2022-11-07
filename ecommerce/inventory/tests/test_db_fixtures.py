63af8d284642ba8763999cc9fc0cee761316ec03
SaadEssam <SaadEssam059@gmail.com>
2022-11-07 16:11:23 +0200

    Category factory

--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -5,7 +5,7 @@ from ecommerce.inventory import models
 @pytest.mark.parametrize(
   "id, name, slug, is_active",
   [
-    (1, "fashion", "fashion", 1)
+    (1, "fashion", "fashion", 1),
     (18, "trainers", "trainers", 1),
     (35, "baseball", "baseball", 1),
   ],
